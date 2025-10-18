a1d817f9546d18221b9ad5b5655cb2ef3f6a8f04
Natalie Miller <nxm2@cornell.edu>
2025-10-18 14:50:53 -0400

    Scraped text of privacy policies from initial apps

---
+++ scraping-policies.R
@@ -0,0 +1,28 @@
+setwd("C:/Users/natal/OneDrive/Documents/GitHub/consumer-data/data")
+
+library(tidyverse)
+library(rvest)
+library(stringr)
+
+if(!dir.exists("privacy_policies")){
+  dir.create("privacy_policies")
+}
+
+app.list <- read.csv("Privacy Policies.csv") |>
+  select(-X)
+
+get.text <- function(link, name, folder = "privacy_policies") {
+  tryCatch({
+    page <- read_html(link)
+    text <- page |> html_nodes("p") |> html_text()
+    text <- paste(text, collapse = "\n\n")
+    file.name <- paste0(folder, "/", make.names(name), ".txt")
+    writeLines(text, file.name)
+  }, error = function(e){
+    return(NA)
+  })
+}
+
+for(i in seq_len(nrow(app.list))){
+  get.text(app.list$Privacy.Policy.Link[i], app.list$App.Name[i])
+}
