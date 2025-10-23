377c598d0446e583027005b9372cde39e25e445b
Natalie Miller <nxm2@cornell.edu>
2025-10-23 12:14:58 -0400

    commented scraping script

--- scripts/scraping-policies.R
+++ scripts/scraping-policies.R
@@ -4,11 +4,13 @@ library(tidyverse)
 library(rvest)
 library(stringr)
 
+# Read in list of privacy policies and links; clean app names
 app.list <- read.csv("privacy-policy-list.csv") |>
   select(-Secondary.Link, -Manually.Saved.) |>
   mutate(App.Name = str_replace_all(App.Name, "[^\\p{L}\\s]", "")) |>
   mutate(App.Name = str_squish(App.Name))
 
+# Write function to scrape policies and save them as individual .txt files
 get.text <- function(link, name, folder = "privacy_policies") {
   tryCatch({
     page <- read_html(link)
@@ -34,6 +36,7 @@ get.text <- function(link, name, folder = "privacy_policies") {
   })
 }
 
+# Apply function to scrape policies
 app.list$file_saved <- FALSE
 
 for (i in seq_len(nrow(app.list))) {
@@ -44,6 +47,11 @@ for (i in seq_len(nrow(app.list))) {
   app.list$file_saved[i] <- success
 }
 
+# NOTE: some scrapes were unsuccessful or returned text that was not the privacy policy.
+# For those, we manually copied text from the webpages and saved the policies to the
+# manual_privacy_policies folder.
+
+# Combine all the individual .txt files into one dataframe
 file.names <- c(
   list.files("privacy_policies", pattern="\\.txt", full.names=T), 
   list.files("manual_privacy_policies", pattern="\\.txt", full.names=T)
@@ -54,4 +62,5 @@ policy.texts <- tibble(
   app = basename(file) |> str_remove("\\.txt$") |> str_trim(),
   text = map_chr(file.names, read_file))
 
+# Export combined dataframe
 write.csv(policy.texts, "all_privacy_policies.csv", row.names = FALSE)  
