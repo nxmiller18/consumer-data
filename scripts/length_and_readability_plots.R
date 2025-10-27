e21c79ffa80acb7045dc0ac676faf432a927d250
Natalie Miller <nxm2@cornell.edu>
2025-10-27 00:27:09 -0400

    readability eda

--- scripts/length_and_readability_plots.R
+++ scripts/length_and_readability_plots.R
@@ -1,12 +1,14 @@
 # AUTHOR: Natalie Miller
 # PURPOSE: Basic plots on length and readability
-# OUTPUTS:
+# OUTPUTS: length_dists_by_category.png, read_time_vs_readability.png
 
 setwd("~/GitHub/consumer-data/data")
 
 library(tidyverse)
 library(readxl)
 library(stringr)
+library(quanteda)
+library(quanteda.textstats)
 
 # Read in data frames for average length by category visualization
 policy.texts <- read.csv("all_privacy_policies.csv")
@@ -46,3 +48,31 @@ policy_length_dist
 
 # Calculate app policy reading times
 
+merged <- merged |>
+  mutate(time=word.count/238) |>
+  mutate(clean.text=str_squish(text))
+
+policy_corpus <- corpus(merged$clean.text, docnames = merged$app)
+
+readability_scores <- textstat_readability(policy_corpus, measure = c("Flesch", "FOG")) |>
+  rename(app=document)
+
+merged_readability <- merged |>
+  left_join(readability_scores, by="app")
+
+readability_scatter <- 
+  ggplot(merged_readability, aes(x=time, y=Flesch, color=Category)) +
+  geom_point(alpha=0.7, size=2) +
+  theme_minimal(base_size=12) +
+  labs(
+    title="Reading Time and Readability Scores",
+    x="Average Time to Read Policy (Minutes)",
+    y="Flesch Readability Score (0-100)",
+    color="App Category"
+  )
+
+readability_scatter
+
+# Export graphs
+ggsave("../figures/length_dists_by_category.png", plot=policy_length_dist)
+ggsave("../figures/read_time_vs_readability.png", plot=readability_scatter)
